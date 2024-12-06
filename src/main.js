9628e6c44f48bae61bf1c94a76a74a3541d92fef
Daniel Agur <danielagur08@gmail.com>
2024-12-05 20:07:25 -0500

    committed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import * as CANNON from 'cannon'
+import * as CANNON from 'cannon-es'
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 const w = window.innerWidth;
@@ -8,6 +8,9 @@ const ballDX = -0.0001;
 const ballDZ = 0.0001;
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
+
+const clock = new THREE.Clock();
+let delta;
 
 const loader = new THREE.TextureLoader();
 const texture = loader.load('resources/alien.jpg');
@@ -22,6 +25,9 @@ camera.position.z = 15; // Adjusted camera position
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x0077ff); // Set background color
+
+const world = new CANNON.World();
+world.gravity.set(0, -9.81, 0);
 
 class Ball {
   constructor(x, y, z, r) {
@@ -53,6 +59,10 @@ class Ball {
     this.mesh.rotateZ(THREE.MathUtils.degToRad(90));
     this.light = new THREE.DirectionalLight(0xffffff, 5);
     this.light.position.set(this.x, this.y+10, this.z + 5);
+    // this.shape = new CANNON.Sphere(this.r);
+    // this.body = new CANNON.Body({mass: 1});
+    // this.body.addShape(this.shape);
+    // this.body.position.copy(this.mesh.position);
   }
 }
 
@@ -68,38 +78,61 @@ class Ground {
     });
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.mesh.receiveShadow = true;
+    this.mesh.position.set(0, 0, 0);
+    // this.shape = new CANNON.Plane();
+    // this.body = new CANNON.Body({mass: 0});
+    // this.body.addShape(this.shape);
+    // this.body.position.copy(this.mesh.position);
   }
 }
 
-let player = new Ball(0, 0, 0, 1);
+let player = new Ball(0, 50, 0, 1);
 player.load();
 scene.add(player.mesh, player.light);
-camera.position.y = player.mesh.position.y;
-camera.position.x = player.mesh.position.x;
-camera.position.z = player.mesh.position.z + 10;
-
+const sphereShape = new CANNON.Sphere(player.r);
+const sphereBody = new CANNON.Body({mass: 1});
+sphereBody.addShape(sphereShape);
+world.addBody(sphereBody);
+sphereBody.position.set(player.x, player.y, player.z);
 
 let ground = new Ground(20, 1000);
 ground.load();
-ground.mesh.rotation.x = 80;
-ground.mesh.position.set(0, player.mesh.position.y - player.r, 0)
+ground.mesh.rotateX(-Math.PI / 2)
+
+ground.mesh.position.set(0, 0, 0);
 
 scene.add(ground.mesh);
 
-
-
+const planeShape = new CANNON.Plane();
+const planeBody = new CANNON.Body({mass: 0});
+planeBody.addShape(planeShape);
+planeBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+world.addBody(planeBody);
 camera.lookAt(
   player.mesh.position.x,
   player.mesh.position.y,
   player.mesh.position.z,
 )
-function animate(t = 0) {
-    // player.mesh.rotation.x = t * -0.001;
-    camera.position.set(0, player.mesh.position.y + 5, player.mesh.position.z + 10);
-    //camera.position.z = player.mesh.position.z + 10;
-    //ground.mesh.rotation.x = t * 0.001;
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
+function animate() {
+  //document.body.innerHTML = sphereBody.position;
+  requestAnimationFrame(animate);
+  // player.mesh.rotation.x = t * -0.001;
+  delta = Math.min(clock.getDelta(), 0.1)
+  
+  camera.position.set(0, 5, player.mesh.position.z + 10);
+  //camera.position.z = player.mesh.position.z + 10;
+  //ground.mesh.rotation.x = t * 0.001;
+  camera.lookAt(
+    player.mesh.position.x,
+    player.mesh.position.y,
+    player.mesh.position.z
+  );
+  player.mesh.position.copy(sphereBody.position);
+  player.mesh.quaternion.copy(sphereBody.quaternion);
+  renderer.render(scene, camera);
+  if (delta > 0) {
+    world.step(delta);
+  }
 }
 
 
