3ba956871b5997f86804a98dfbedf19fe8358c03
Daniel Agur <danielagur08@gmail.com>
2024-12-10 14:50:43 -0500

    committed

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import * as THREE from 'three';
 import * as CANNON from 'cannon-es';
 import { CSS2DObject, CSS2DRenderer } from 'three/examples/jsm/Addons.js';
 
+
+document.createElement("div").id = "ins";
+document.getElementById("ins").innerHTML = "<div style='background-color: white; padding: 15px; border-radius: 30px'>Press SPACE to jump over red blocks,<br>answer questions,<br>and gain points.<br>BE ALIGNED WITH THE BLOCKS!</div><br><button style='background-color:green; border-radius: 30px; padding: 10px;' id=\"startGame\">Start</button>";
+
+const fixedTimeStep = 1.0/60.0;
+let accumulator = 0;
+
 const labelRenderer = new CSS2DRenderer();
 labelRenderer.setSize(window.innerWidth, window.innerHeight);
 labelRenderer.domElement.style.position = 'absolute';
@@ -9,7 +16,6 @@ labelRenderer.domElement.style.top = '0px';
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 const w = window.innerWidth;
 const h = window.innerHeight;
-const ballDX = -0.0001;
 // const ballDZ = 0.0001;
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
@@ -17,12 +23,7 @@ document.body.appendChild(labelRenderer.domElement);
 const clock = new THREE.Clock();
 let delta;
 // force for jump
-const impulseJump = new CANNON.Vec3(0, 6, 0);
-// force for move right
-const pX = new CANNON.Vec3(5, 0, 0);
-const nX = new CANNON.Vec3(-5, 0, 0);
-// zero force
-const zeroV = new CANNON.Vec3(0, 0, 0);
+const impulseJump = new CANNON.Vec3(0, 8, 0);
 
 const loader = new THREE.TextureLoader();
 const texture = loader.load('resources/alien.jpg');
@@ -39,7 +40,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x0077ff); // Set background color
 
 const world = new CANNON.World();
-world.gravity.set(0, -12, 0);
+world.gravity.set(0, -9.8, 0);
 
 class Ball {
   constructor(x, y, z, r) {
@@ -106,7 +107,6 @@ const sphereBody = new CANNON.Body({mass: 1});
 sphereBody.addShape(sphereShape);
 world.addBody(sphereBody);
 sphereBody.position.set(player.x, player.y, player.z);
-const count = 0;
 let ground = new Ground(15, 1000);
 ground.load();
 ground.mesh.rotateX(-Math.PI / 2)
@@ -132,17 +132,8 @@ camera.lookAt(
 // labelRenderer.setSize(window.innerWidth, window.innerHeight);
 // labelRenderer.domElement.style.position = "absolute";
 // document.body.appendChild(labelRenderer.domElement);
-const instructions = document.createElement("div");
-instructions.id = "ins";
-instructions.innerHTML = "Press SPACE to jump over Red,<br>hit green to answer a question and<br>score a point<br><button style='background-color:green' onclick='start();'>Start</button>";
-const objectInstructions = new CSS2DObject(instructions);
-
-const innerScript = document.createElement("script");
-innerScript.innerHTML = "function start() {document.getElementById(\"ins\").innerHTML = \"\";}"
-const objectInnerScript = new CSS2DObject(innerScript);
-// start button
-player.mesh.add(objectInnerScript);
-
+let instructions = document.getElementById("ins");
+let objectInstructions = new CSS2DObject(instructions);
 objectInstructions.position.set(
   player.mesh.position.x - 10,
   player.mesh.position.y + 10,
@@ -152,16 +143,19 @@ ground.mesh.add(objectInstructions);
 var axis = new CANNON.Vec3(0, 0, 1);
 var angle = Math.PI/2;
 sphereBody.quaternion.setFromAxisAngle(axis, angle);
-
+// check if game has started
+let started = document.getElementById(instructions.id).innerHTML == "";
 // ball movements
-const stopBallF = sphereBody.velocity.clone().negate().scale(1);
+let stopBallF = sphereBody.velocity.clone().negate().scale(1);
 let stopped = false;
+// let stopped = false;
 function animate(t = 0) {
   //document.body.innerHTML = sphereBody.position;
   requestAnimationFrame(animate);
   // player.mesh.rotation.x = t * -0.001;
   delta = Math.min(clock.getDelta(), 0.1);
   labelRenderer.render(scene, camera);
+  accumulator += delta;
   camera.position.set(0, 5, player.mesh.position.z + 8);
   //camera.position.z = player.mesh.position.z + 10;
   //ground.mesh.rotation.x = t * 0.001;
@@ -170,8 +164,12 @@ function animate(t = 0) {
     player.mesh.position.y,
     player.mesh.position.z
   );
+  while (accumulator >= fixedTimeStep) {
+    world.step(fixedTimeStep);
+    accumulator -= fixedTimeStep;
+  }
   player.mesh.position.copy(sphereBody.position);
-  player.mesh.rotation.set(t * -0.003, 0, Math.PI/2);
+  player.mesh.rotation.set(t * -0.005, 0, Math.PI/2);
   //player.mesh.setRotationFromAxisAngle(axis, angle);
   //player.mesh.quaternion.copy(sphereBody.quaternion);
   player.mesh
@@ -185,45 +183,50 @@ function animate(t = 0) {
     player.mesh.position.z
   );
   if (stopped) {
-    sphereBody.velocity.set(0, 0, 0);
+    sphereBody.velocity.setZero();
   }
-  
 }
 
 animate();
 
-document.addEventListener("keypress", jump, false);
+document.addEventListener("keydown", jump, false);
 // jump function
 function jump(event) {
   if (event.which == 32) {
-    if (player.mesh.position.y < 1.5) {
+    if (player.mesh.position.y <= 1) {
+      stopped = false;
       sphereBody.applyImpulse(impulseJump);
     }
-    
   }
 }
 
 document.addEventListener("keydown", move, false);
 // move functions
-
 function move(event) {
-  if (event.which == 37) {
-    stopped = false;
-    sphereBody.velocity.set(-5, 0, 0);
-    console.log("37")
-  } if (event.which == 39) {
-    stopped = false;
-    sphereBody.velocity.set(5, 0, 0);
-    console.log("39");
+  if (started) {
+    if (event.which == 37) {
+      stopped = false
+      sphereBody.velocity.set(-20, 0, 0);
+    } if (event.which == 39) {
+      stopped = false;
+      sphereBody.velocity.set(20, 0, 0);
+    }
   }
 }
 
 document.addEventListener("keyup", stop, false);
 function stop(event) {
-  if (event.which == 37 || event.which == 39) {
-    stopped = true;
-    sphereBody.applyForce(stopBallF);
+  if (started) {
+    if ((event.which == 37 || event.which == 39) && sphereBody.position.y <= 1) {
+      stopped = true;
+    }
   }
+}
+
+document.getElementById("startGame").addEventListener("click", start, false);
+function start(event) {
+  document.getElementById("ins").innerHTML = ""; 
+  started = true;
 }
 
 // Handle window resize
@@ -235,3 +238,6 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
     //labelRenderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+
+// block spawner
